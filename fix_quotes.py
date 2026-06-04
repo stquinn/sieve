ec92d364b6e1688bae3702295b78b736e76d80f5
stephen <stephen.quinn2@gmail.com>
2026-06-04 22:47:15 +0100

    remove rubbush

--- fix_quotes.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('frontend/src/static/editor.js', 'r') as f:
-    content = f.read()
-
-content = content.replace("question: question || ',", "question: question || '',")
-
-with open('frontend/src/static/editor.js', 'w') as f:
-    f.write(content)
